5816d0d437f1c8ba51f8e6453018abd7ea484671
cjdev544 <cjdev544@gmail.com>
2023-05-22 15:47:56 -0400

    basic routs with lazy and suspense ready

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,6 @@
 import { NavLink, Outlet } from 'react-router-dom'
 import Logo from '../assets/react.svg'
+import { routes } from './routerPaths'
 
 export default function Root() {
   return (
@@ -7,30 +8,16 @@ export default function Root() {
       <nav>
         <img className='logo' src={Logo} alt='Logo' />
         <ul>
-          <li>
-            <NavLink
-              className={({ isActive }) => (isActive ? 'nav-active' : '')}
-              to='/lazy1'
-            >
-              LazyPage1
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) => (isActive ? 'nav-active' : '')}
-              to='/lazy2'
-            >
-              LazyPage2
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) => (isActive ? 'nav-active' : '')}
-              to='/lazy3'
-            >
-              LazyPage3
-            </NavLink>
-          </li>
+          {routes.map((route) => (
+            <li key={route.path}>
+              <NavLink
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+                to={route.path}
+              >
+                {route.name}
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </nav>
       <Outlet />
--- src/routes/routerPaths.tsx
+++ src/routes/routerPaths.tsx
@@ -1,30 +1,52 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createBrowserRouter } from 'react-router-dom'
 import Root from './Root'
-import LazyPage1 from '../01-lazyload/pages/LazyPage1'
-import LazyPage2 from '../01-lazyload/pages/LazyPage2'
-import LazyPage3 from '../01-lazyload/pages/LazyPage3'
+import { Suspense, lazy } from 'react'
+
+interface Route {
+  path: string
+  element: JSX.Element
+  name: string
+}
+
+const Lazy1 = lazy(() => import('../01-lazyload/pages/LazyPage1'))
+const Lazy2 = lazy(() => import('../01-lazyload/pages/LazyPage2'))
+const Lazy3 = lazy(() => import('../01-lazyload/pages/LazyPage3'))
+
+export const routes: Route[] = [
+  {
+    path: '/lazy1',
+    element: (
+      <Suspense fallback={<h3>Cargando...</h3>}>
+        <Lazy1 />
+      </Suspense>
+    ),
+    name: 'Lazy Page 1',
+  },
+  {
+    path: '/lazy2',
+    element: (
+      <Suspense fallback={<h3>Cargando...</h3>}>
+        <Lazy2 />
+      </Suspense>
+    ),
+    name: 'Lazy Page 2',
+  },
+  {
+    path: '/lazy3',
+    element: (
+      <Suspense fallback={<h3>Cargando...</h3>}>
+        <Lazy3 />
+      </Suspense>
+    ),
+    name: 'Lazy Page 3',
+  },
+]
 
 export const routerPaths = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    children: [
-      {
-        path: '/lazy1',
-        element: <LazyPage1 />,
-      },
-      {
-        path: '/lazy2',
-        element: <LazyPage2 />,
-      },
-      {
-        path: '/lazy3',
-        element: <LazyPage3 />,
-      },
-      {
-        path: '/*',
-        element: <h1>Other route</h1>,
-      },
-    ],
+    children: routes,
   },
 ])
